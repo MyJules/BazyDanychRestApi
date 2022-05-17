805b7f4ae5469bb44bf07498e332efb2419cb99c
MyJules <mudryk200@gmail.com>
2022-05-17 12:55:18 +0200

    Added SQLite DB

--- SpringBackend/src/main/java/com/rest/SpringBackend/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.rest.SpringBackend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- SpringBackend/src/main/java/com/rest/SpringBackend/exceptions/StudentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.rest.SpringBackend.exceptions;
-
-public class StudentNotFoundException extends RuntimeException{
-
-    public StudentNotFoundException(Long id) {
-        super("Could not find student with id: " + id);
-    }
-}
